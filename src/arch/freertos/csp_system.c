4d266787dbd3a52a00282077117431dd172429bb
joalopez1206 <joalopez@ug.uchile.cl>
2023-04-20 15:05:59 -0400

    changes to csp system

--- src/arch/freertos/csp_system.c
+++ src/arch/freertos/csp_system.c
@@ -14,3 +14,26 @@ __attribute__((weak)) uint32_t csp_memfree_hook(void) {
 __attribute__((weak)) unsigned int csp_ps_hook(csp_packet_t * packet) {
 	return 0;
 }
+
+void csp_reboot_hook(void) {
+
+    extern void __attribute__((weak)) cpu_set_reset_cause(unsigned int);
+    if (cpu_set_reset_cause)
+        cpu_set_reset_cause(1);
+
+    extern void __attribute__((weak)) cpu_reset(void);
+    if (cpu_reset) {
+        cpu_reset();
+        while (1);
+    }
+
+    printf("Failed to reboot");
+}
+void csp_shutdown_hook(void){
+    extern void __attribute__((weak)) cpu_shutdown(void);
+    if (cpu_shutdown) {
+        cpu_shutdown();
+        while (1);
+    }
+    printf("Failed to reboot");
+}
